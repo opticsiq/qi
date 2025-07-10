8c52342f5b8e4882a12379175859f814568f8718
Muhaimin <cx1@live.ca>
2025-07-10 16:20:57 +0300

    fix: add basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/qi">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/admin" element={<Admin />} />
